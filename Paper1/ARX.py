caae79d9d16e98b17588d4522883d66b887bb1b9
althausLuca <luca.althaus@students.unibe.ch>
2021-09-27 15:53:39 +0200

    AR and ARX from paper1 implemented, ols not 100% clear

---
+++ Paper1/ARX.py
@@ -0,0 +1,48 @@
+##ARX example from the paper
+
+from scipy.linalg import lstsq as ls
+import numpy as np
+from statsmodels.tsa.ar_model import AutoReg
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression as LR
+
+
+x_original = np.array([6, 10, 9.6, 8.3, 7.7, 5.4, 5.6, 5.9, 6.3, 6.8, 7.5, 8.5])
+x = x_original.copy()
+y = np.array([6, 5.6, 5.4, 5.2, 5.4, 5.4, 5.6, 5.9, 6.3, 6.8, 7.5, 8.5])
+labels = [0,1,2,5,11]
+
+
+
+#train the arameter with the already corrected x
+x[labels] = y[labels]
+
+x_right =  y[1:]-x[1:]
+x_left = (y-x)[:-1]
+D = np.concatenate( (np.zeros(len(x_left)),x_left) ,axis=0 ).reshape(2,-1).T
+theta  = ls(D,x_right)[0]
+
+
+
+theta[1] = 0.5   #problem here is the fit rouned ?
+phi = theta[1]
+
+tau = 0.1
+x = x_original
+
+
+print(10)
+
+for i in range(1,len(x_right)):
+    if  i not in labels:
+        y_prime = x[i]+phi*(y[i-1] - x[i-1])
+        if i == 3:
+            print(y_prime , "y3" ,  x[i-1] , y[i-1] , "xi" , x[i])
+        if abs(x[i] - (x_left[i - 1] * theta[1] + theta[0])) > tau :
+            y[i] = y_prime
+
+#y = x
+
+# {6, 5.6, 5.4, 5.52, 5.64, 5.4, 5.6, 5.72, 5.84, 5.97, 6.10, 8.5}.
+
+
