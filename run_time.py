8f7a355779cca160216633909a2ba30c7185e273
althausluca <luca.althaus@students.unibe.ch>
2022-09-09 13:28:50 +0200

    runtime file updated

--- run_time.py
+++ run_time.py
@@ -5,7 +5,7 @@ import timeit
 
 sc.MAX_N_ROWS  = 200000
 start = timeit.timeit()
-input = "-scen ts_len -data all -rn runtime  -anom shift -alg imr,screen -e full_rmse -rtn 20"  # algox" # "-scen vary_ts_length  -col 0  -data YAHOO.csv -anom a -algo 1 "
+input = "-scen ts_len -data all -rn runtime  -anom shift -alg all -e full_rmse -rtn 20"  # algox" # "-scen vary_ts_length  -col 0  -data YAHOO.csv -anom a -algo 1 "
 run.main(input)
 end = timeit.timeit()
 print(end-start)
@@ -15,7 +15,7 @@ sc.MAX_N_ROWS  = 10000
 sc.MAX_N_COLS  = 20
 
 start = timeit.timeit()
-input = "-scen ts_nbr,cts_nbr -data all -rn runtime -anom shift -alg imr,screen -e full_rmse -rtn 20"
+input = "-scen ts_nbr,cts_nbr -data all -rn runtime -anom shift -alg all -e full_rmse -rtn 20"
 run.main(input)
 end = timeit.timeit()
 print(end-start)
