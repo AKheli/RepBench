74648553dae287ee86d4cd0d1a49dd4d26a4350d
althausluca <luca.althaus@students.unibe.ch>
2022-12-12 21:03:48 +0100

    Requirements moved

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 sudo apt install python-dev
 sudo apt install python3-pip
+
+python3 -m venv ../venv
+source ../venv/bin/activate
+
 pip3 install toml
 pip3 install pandas
 pip3 install matplotlib
 pip3 install prettytable
 pip3 install scikit-optimize
 pip3 install pyinform
+
+# web Requriemnts
+pip3 isntall WebApp/requirements.txt
