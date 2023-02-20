ac177bfb451bd130942c702248deb527108f96e0
althausluca <luca.althaus@students.unibe.ch>
2023-02-20 17:53:44 +0100

    Export option added

--- WebApp/static/js/export.js
+++ WebApp/static/js/export.js
@@ -17,7 +17,26 @@ const exportData = function () {
     a.click();
 }
 
+const exportSyntheticData = function () {
+    console.log(injectedSeries.map(s => s.originalData))
+    console.log("exporting injected series")
+    let blob = new Blob(addHeader(CSVzip(...injectedSeries.map(s => s.originalData))), {type: 'text/csv'});
+    let a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = 'myFile.csv';
+    document.body.appendChild(a);
+    a.click();
+}
+
 
 // bind exportData to the button with id exportOriginalData
 const exportDataBtn = document.getElementById("exportOriginalData")
 exportDataBtn.addEventListener("click",exportData)
+
+//todo check order of creation of synthetic series
+const exportInjectedDataBtn = document.getElementById("exportInjectedData")
+exportInjectedDataBtn.addEventListener("click",exportSyntheticData)
+if(injectedSeries === null || injectedSeries.length===0){
+    console.log(injectedSeries,"AAAA")
+    exportInjectedDataBtn.style.display="none"
+}
